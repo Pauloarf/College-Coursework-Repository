1df2c25f16790661075cdc8e71a85edccec1286f
pauloarf <pauloferreira.vnc@gmail.com>
2024-02-26 16:10:12 +0000

    feat(poo): added more exercises from "Ficha2"

--- Object-Oriented-Programming/Fichas/Ficha2/src/IntArray.java
+++ Object-Oriented-Programming/Fichas/Ficha2/src/IntArray.java
@@ -1,19 +1,48 @@
+import java.util.Arrays;
+
 public class IntArray {
 
+    public int n_elements;
     public int[] array;
 
-    public IntArray(int[] arr){
+    public IntArray(int n_elements, int[] arr){
         this.array = arr; //Passar o apontador para o array ou o seu clone
+        this.n_elements = n_elements;
     }
 
-    public int MinimumValue(int[] arr){
-        if(arr.length < 1) return Integer.MIN_VALUE; // O que fazemos se o array for vazio
+    public int MinimumValue(){
+        if(this.array.length < 1) return Integer.MIN_VALUE; // O que fazemos se o array for vazio
         else {
-            int min = arr[0];
-            for (int i : arr) {
-                if (min > i) min = i;
+            int min = this.array[0];
+            for (int i = 0; i < n_elements; i++) {
+                if (min > this.array[i]) min = this.array[i];
             }
             return min;
         }
+    }
+
+    public int[] arrFromAtoB(int a, int b){
+        return Arrays.copyOfRange(this.array, a, b);
+    }
+
+    public int[] elementosComuns(int[] arr, int arr_size){
+        Arrays.sort(arr, 0, arr_size);
+        Arrays.sort(this.array, 0, this.n_elements);
+
+        int newArrSize = Math.min(arr_size, this.n_elements);
+
+        int[] newArray = new int[newArrSize];
+        int index = 0;
+        for(int i = 0; i < n_elements; i++){
+            for(int j = 0; j < arr_size; j++){
+                if(this.array[i] > arr[j]) continue;
+                if (this.array[i] == arr[j]) {
+                    newArray[i] = this.array[index];
+                    index++;
+                    break;
+                }
+            }
+        }
+        return newArray;
     }
 }
--- Object-Oriented-Programming/Fichas/Ficha2/src/TestePrograma.java
+++ Object-Oriented-Programming/Fichas/Ficha2/src/TestePrograma.java
@@ -1,7 +1,34 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class TestePrograma {
+    public static List<Object> makeNewArray(){
+        List<Object> result = new ArrayList<>();
+        Scanner sc2 = new Scanner(System.in);
+
+        System.out.println("Insira o tamanho do array:");
+        int arraySize = sc2.nextInt();
+        int n_elements = 0;
+
+        sc2.nextLine(); // Consume the newline character after reading the integer
+
+        int[] arr = new int[arraySize];
+        System.out.println("Insira os numeros a serem inseridos no array:");
+        String numeros = sc2.nextLine();
+        Scanner nsc = new Scanner(numeros);
+
+        for (int i = 0; i < arraySize && nsc.hasNextInt(); i++) {
+            arr[i] = nsc.nextInt();
+            n_elements++;
+        }
+        result.add(arr);
+        result.add(n_elements);
+        nsc.close();
+        return result;
+    }
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
@@ -11,21 +38,25 @@ public class TestePrograma {
 
         switch (ex){
             case 1:
-                System.out.println("Insira o tamanho do array:");
-                int arraySize = sc.nextInt();
-                sc.nextLine(); // Consume the newline character after reading the integer
+                List<Object> result = makeNewArray();
+                int[] arr = (int[]) result.get(0);
+                int n_elements = (int) result.get(1);
 
-                int[] arr = new int[arraySize];
-                System.out.println("Insira os numeros a serem inseridos no array:");
-                String numeros = sc.nextLine();
-                Scanner nsc = new Scanner(numeros);
-
-                for (int i = 0; i < arraySize && nsc.hasNextInt(); i++) {
-                    arr[i] = nsc.nextInt();
-                }
-
-                IntArray intArray = new IntArray(arr);
+                IntArray intArray = new IntArray(n_elements,arr);
                 System.out.println(Arrays.toString(intArray.array));
+                System.out.println("Valor minimo no array: " + intArray.MinimumValue());
+                System.out.println("Escolha dois indices de modo a obter um array entre esses valores:");
+                System.out.println("Indice a:");
+                int a = sc.nextInt();
+                System.out.println("Indice b:");
+                int b = sc.nextInt();
+                System.out.println(Arrays.toString(intArray.arrFromAtoB(a, b)));
+
+                List<Object> result2 = makeNewArray();
+                int[] arr2 = (int[]) result2.get(0);
+                int n_elements2 = (int) result2.get(1);
+
+                System.out.println(Arrays.toString(intArray.elementosComuns(arr2, n_elements2)));
                 break;
             case 2:
                 break;
