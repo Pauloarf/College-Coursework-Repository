cbb9a0b4d755654b499340bc869882572f79918f
pauloarf <pauloferreira.vnc@gmail.com>
2024-03-21 17:25:43 +0000

    feat: adds minor changes

---
+++ Object-Oriented-Programming/Fichas/Ficha5/src/test/java/EncomendaTest.java
@@ -0,0 +1,62 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EncomendaTest {
+
+    @Test
+    void calculaValorTotal() {
+    }
+
+    @Test
+    void calculaValorTotal_I() {
+    }
+
+    @Test
+    void calculaValorDesconto() {
+    }
+
+    @Test
+    void calculaValorDesconto_E() {
+    }
+
+    @Test
+    void numeroTotalProdutos() {
+    }
+
+    @Test
+    void existeProdutoEncomenda() {
+    }
+
+    @Test
+    void existeNaEncomenda() {
+    }
+
+    @Test
+    void adicionaLinha() {
+    }
+
+    @Test
+    void removeProduto() {
+    }
+
+    @Test
+    void numProdutos() {
+    }
+
+    @Test
+    void getListaProdutos() {
+    }
+
+    @Test
+    void getListaProdutosAsSet() {
+    }
+
+    @Test
+    void getListaProdutosAsSetI() {
+    }
+
+    @Test
+    void encomendasPorOrdem() {
+    }
+}
