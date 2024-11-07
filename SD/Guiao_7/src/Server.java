7fa7bff8ffc559188bfd6e6a0d3cf9e8763444bb
Pauloarf <pauloferreira.vnc@gmail.com>
2024-11-07 10:40:18 +0000

    feat(sd): adds Guiao 7

---
+++ SD/Guiao_7/src/Server.java
@@ -0,0 +1,89 @@
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+import static java.util.Arrays.asList;
+
+class ContactManager {
+    private Lock l = new ReentrantLock();
+    private Condition con = l.newCondition();
+    private HashMap<String, Contact> contacts = new HashMap<>();
+
+    // @TODO
+    public void update(Contact c) {
+        try {
+            l.lock();
+            contacts.put(c.name(), c);
+        } finally {
+            l.unlock();
+        }
+    }
+
+    // @TODO
+    public ContactList getContacts() {
+        try{
+            l.lock();
+            ContactList list = new ContactList();
+            list.addAll(contacts.values());
+            return list;
+        } finally {
+            l.unlock();
+        }
+    }
+}
+
+class ServerWorker implements Runnable {
+    private Socket socket;
+    private ContactManager manager;
+
+    public ServerWorker(Socket socket, ContactManager manager) {
+        this.socket = socket;
+        this.manager = manager;
+    }
+
+    // @TODO
+    @Override
+    public void run() {
+        try {
+            DataInputStream input = new DataInputStream(this.socket.getInputStream());
+            Contact newContact = Contact.deserialize(input);
+
+            this.manager.update(newContact);
+
+            System.out.println(this.manager.getContacts().toString());
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
+
+
+public class Server {
+
+    public static void main (String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(12345);
+        ContactManager manager = new ContactManager();
+        // example pre-population
+        manager.update(new Contact("John", 20, 253123321, null, asList("john@mail.com")));
+        manager.update(new Contact("Alice", 30, 253987654, "CompanyInc.", asList("alice.personal@mail.com", "alice.business@mail.com")));
+        manager.update(new Contact("Bob", 40, 253123456, "Comp.Ld", asList("bob@mail.com", "bob.work@mail.com")));
+
+        while (true) {
+            Socket socket = serverSocket.accept();
+            Thread worker = new Thread(new ServerWorker(socket, manager));
+            worker.start();
+        }
+    }
+
+}
